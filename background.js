19afb90dca8f848194e946cef2fdfcf1e9982de6
nir <movsho3@gmail.com>
2019-04-11 10:48:19 +0300

    version 2

--- background.js
+++ background.js
@@ -1,16 +1,103 @@
 var url="http://localhost:5555/test";
-var youtubeUrl="https://www.youtube.com/embed/";
+var youtubeUrl="http://localhost:4000/?url=https://youtu.be/";
 var response=undefined;
+var nextSongPlay = null;
+var songsPlayers = [];
+var needToSend = true;
+var iframe = document.createElement('iframe');
+var nowPlaying = null;
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.type == "playSong") {
-        callServer(request.tabs);
-        //response.url=youtubeUrl+response.url;
-        var nir ={};
-        sendResponse(nir);
+		nextSong();
     }
+	if (request.type == "pause") {
+		 var message = new Object();
+		 message.type = "pause";
+		iframe.contentWindow.postMessage(message, '*'); 
+    }
+	if (request.type == "loaded") {
+		var message = new Object();
+		message.type = "getTime";
+		iframe.contentWindow.postMessage(message, '*'); 
+		sendResponse({song: nowPlaying});		
+    }
+	if (request.type == "seekTo") {
 
-
+	}
 });
+function receiveMessage(event)
+{
+  var data = event.data;
+  if (data.type === 'ended')
+  {
+	 nextSong();
+  }
+  if (data.type === 'currentTime')
+  {
+	  nowPlaying.duration = data.duration;
+	  nowPlaying.playing=data.playing;
+	  chrome.runtime.sendMessage({type: "seekTo",song: nowPlaying}, function(res) {
+       });
+  }
+}
+function nextSong() 
+{
+  if ( nextSongPlay == null )
+  {
+	  if (needToSend == true )
+		  initiateNext();
+	  needToSend = true;
+  }
+  else
+  {
+		sendToClientAndInitiateSong(nextSongPlay); 
+		nextSongPlay=null;
+		needToSend = false;
+		initiateNext();
+  } 
+}
+function sendToClientAndInitiateSong(songToSend)
+{
+		  chrome.runtime.sendMessage({type: "newSong",song: songToSend}, function(res) {
+        });
+	    iframe.setAttribute("src", songToSend.url);
+        document.body.appendChild(iframe);
+		nowPlaying = songToSend;
+		var objTopush = new Object();
+		objTopush.song=nowPlaying.SongName;
+		objTopush.artist=nowPlaying.SongArtist;		
+		songsPlayers.push(objTopush);
+}
+function initiateNext()
+{
+		  chrome.tabs.getAllInWindow(null, function(tabs){
+		var tabArray=[];
+		for (var i = 0; i < tabs.length; i++) {			
+			// send only 1 tab - active tab
+			if (tabs[i].active)
+				tabArray.push(tabs[i].url);
+		}
+		var response= new HttpClient();
+		var objectToServer = new Object();
+		objectToServer.tabs=tabArray;
+		objectToServer.songArray=songsPlayers;
+		response.get(url,function(res){
+			var response1=JSON.parse(res);
+			response=response1;
+			response.id=response.url;
+			response.url=youtubeUrl+response.url;
+			nextSongPlay= response;
+			if (needToSend)
+			{
+				sendToClientAndInitiateSong(nextSongPlay);
+				nextSongPlay = null ;
+				needToSend = false;
+				initiateNext();
+			}
+		},objectToServer);	
+	});
+}
+window.addEventListener("message", receiveMessage, false);
 var HttpClient = function ()
 {
     this.get = 	function (theUrl, callback,data)
@@ -24,18 +111,4 @@ var HttpClient = function ()
         console.log(JSON.stringify(data));
         xmlHttp.send(JSON.stringify(data));
     }
-}
-function callServer(tabs) {
-    var response= new HttpClient();
-    response.get(url,function(res){
-        var response1=JSON.parse(res);
-        var ur="https://www.youtube.com/watch?v="+response1.url
-        var myChild= window.open(ur,'','width=,height=,resizable=no');
-        myChild.blur();
-        response=response1;
-        response.url=youtubeUrl+response.url;
-        chrome.runtime.sendMessage({type: "newSong",song: response}, function(res) {
-			console.log("here");
-        });
-    },tabs);
 }
--- popup.js
+++ popup.js
@@ -1,15 +1,18 @@
 window.onload=function(){
-  // document.getElementById("button1").addEventListener("click", displayDate);
-  chrome.windows.getAll({populate:true}, getAllOpenWindows);
-
+	document.getElementById("suitMe").addEventListener("click", pause);
+	document.getElementById("nextSong").addEventListener("click", nextSongFunc);
+	chrome.runtime.sendMessage({type: "loaded"},function(response) {
+		if (response.song!=null)
+		{
+			document.getElementById('iframeID').src =response.song.url;
+			var div = document.getElementById('SongName');
+			var text = document.createTextNode(response.song.SongName);
+			div.appendChild(text);
+		}
+	});
 }
 function sendToBackground(tabs){
 	chrome.runtime.sendMessage({type: "playSong",tabs: tabs},function(response) {
-       console.log(response);
-	   document.getElementById('iframeID').src =response.url;
-	   var paragraph = document.getElementById("SongName");
-	  var text = document.createTextNode(response.SongName);
-	  paragraph.appendChild(text);
 	});
 }
 function getAllOpenWindows(winData) {
@@ -27,13 +30,55 @@ function getAllOpenWindows(winData) {
   console.log(tabs);
   sendToBackground(tabs);
 }
+function pause(){   
+	chrome.runtime.sendMessage({type: "pause"},function(response) {
+	});
+	var iframe = document.getElementById('iframeID');
+	var message = new Object();
+	message.type = "pause";
+	iframe.contentWindow.postMessage(message, '*'); 
+}
+function nextSongFunc(){
+	chrome.windows.getAll({populate:true}, getAllOpenWindows);
+}
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.type == "newSong") {
         console.log("here");
         document.getElementById('iframeID').src =request.song.url;
-        var paragraph = document.getElementById("SongName");
-        var text = document.createTextNode(request.song.SongName);
+
+		var div = document.getElementById('SongName');
+		while(div.firstChild){
+			div.firstChild.innerHTML="";
+		}
+		var text = document.createTextNode("Now Playing : "+response.song.SongName);;
+		div.appendChild(text);
     }
-
-
+	   if (request.type == "seekTo") {
+		var toSend= new Object();
+		toSend.type = 'seekTo';
+		toSend.time=request.song.duration;
+		toSend.playing=request.song.playing;
+		seekToTime = toSend;
+    }
 });
+window.addEventListener("message", receiveMessage, false);
+function receiveMessage(event)
+{
+  var data = event.data;
+  if (data.type === 'ready')
+  {
+	 if (seekToTime != null ){
+		var iframe = document.getElementById('iframeID');
+		iframe.contentWindow.postMessage(seekToTime, '*');
+		seekToTime = null;
+	 }
+  }
+   if (data.type === 'sendPause')
+  {
+	 var iframe = document.getElementById('iframeID');
+	var message = new Object();
+	message.type = "pause";
+	iframe.contentWindow.postMessage(message, '*'); 
+  }
+}
+var seekToTime = null ;
